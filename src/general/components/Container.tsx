b33d416027dd24ca4ccf56da04f54e8d3476e2e6
pratovkhushnudbek <khushnudbek.dev@gmail.com>
2024-03-30 13:31:54 +0500

    fixed: build qilishdagi muammo

--- src/general/components/Container.tsx
+++ src/general/components/Container.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import tablet from "../utils/tablet";
-import laptop from "../utils/laptop";
-import desktop from "../utils/desktop";
 
 interface IContainer {
   children?: React.ReactNode;
@@ -13,17 +10,6 @@ const StyledContainer = styled.div`
   max-width: 100%;
   width: 100%;
   padding-inline: 16px;
-  ${tablet} {
-    padding-inline: 20px;
-  }
-  ${laptop} {
-    max-width: 990px;
-    margin: 0 auto;
-    padding: 0;
-  }
-  ${desktop} {
-    max-width: 1100px;
-  }
 `;
 
 export const Container: React.FC<IContainer> = ({ children, className }) => {
